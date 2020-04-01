ccbbbdf45dc432eda3a03e8ab4c18bd7b9fb588f
Muhammad Zulfadli <zulfadlisimatupang48@gmail.com>
2020-04-01 12:34:42 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="BitLy URL Shortener"/>
   </div>
 </template>
 
@@ -21,7 +20,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
 }
